4dff9906192afe9329093ffd08747fc7b792f88a
Oliver Sokol <oliver.sokol@nhi.no>
2024-08-14 13:53:37 +0200

    some from youtube tut

---
+++ dotnetYoutube/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+string friends = "scott and others, but really just scott";
+
+friends = friends.Replace("scott", "damien");
+
+Console.WriteLine(friends);
+
+
+string bob = "bob";
+
+Boolean isBob = bob switch { "bob" => true, _ => false };
+
+Console.WriteLine(isBob);
